6ba2897f98b0a1d540626093df6d0940ea4dc3a6
Fatema Khanom <fatemaurmi2019@gmail.com>
2023-11-11 23:28:45 +0600

    banner section added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Component/Shared/Navbar';
+import Footer from '../Component/Shared/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
