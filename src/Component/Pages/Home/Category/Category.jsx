2b4edba639c3f4a7a297e8e48d40b27d8f6594ba
Fatema Khanom <fatemaurmi2019@gmail.com>
2023-11-13 18:54:57 +0600

    Edit category

--- src/Component/Pages/Home/Category/Category.jsx
+++ src/Component/Pages/Home/Category/Category.jsx
@@ -1,4 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
@@ -12,27 +13,26 @@ const Category = () => {
     return (
         <div className='mt-20'>
         <Swiper
-        slidesPerView={4}
+        slidesPerView={5}
         spaceBetween={30}
-        centeredSlides={true}
         pagination={{
           clickable: true,
         }}
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
-            <img src={slide1} alt="" />
-            <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p>
-         </SwiperSlide>
-        <SwiperSlide><img src={slide2} alt="" />  <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p></SwiperSlide>
-        <SwiperSlide><img src={slide3} alt="" /> <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p> </SwiperSlide>
-        <SwiperSlide><img src={slide4} alt="" />  <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p></SwiperSlide>
-        <SwiperSlide><img src={slide5} alt="" />  <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p></SwiperSlide>
-        
+       <SwiperSlide>
+<img src={slide1} alt="" />
+<p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p>
+</SwiperSlide>
+<SwiperSlide><img src={slide2} alt="" />  <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p></SwiperSlide>
+<SwiperSlide><img src={slide3} alt="" /> <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p> </SwiperSlide>
+<SwiperSlide><img src={slide4} alt="" />  <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p></SwiperSlide>
+<SwiperSlide><img src={slide5} alt="" />  <p className='text-4xl uppercase text-center -mt-16 text-white'>salad</p></SwiperSlide>
       </Swiper>
         </div>
     );
 };
 
 export default Category;
+
