ce031633278d83336706eda82093469bf4ed4f7a
Fatema Khanom <fatemaurmi2019@gmail.com>
2023-11-13 23:26:47 +0600

    add featured items section

--- src/Component/Pages/Home/Featured/Featured.jsx
+++ src/Component/Pages/Home/Featured/Featured.jsx
@@ -1,18 +1,59 @@
+// import SectionTitle from "../../../Shared/SectionTitle/SectionTitle";
+// import featuredimg from "../../../../assets/home/featured.jpg"
+
+// const Featured = () => {
+//     return (
+//         <div className="featured-item">
+//             <SectionTitle subHeading={"Check in Out"} heading={"Featured Items"}>
+                  
+//             </SectionTitle>
+//             <div className=" flex justify-center items-center py-8 px-16">
+//                 <img src={featuredimg} className="w-[400px]" alt="" />
+//                 <div className="md:ml-10 text-start">
+//                     <p className="">March 20, 2023</p>
+//                     <p className="">WHERE CAN I GET SOME?</p>
+//                     <p className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, repellat. Est distinctio, magnam soluta dolore accusantium repudiandae ratione consequatur eius? Qui, expedita! At rerum consequuntur consectetur repellat harum unde temporibus!</p>
+//                     <button className="btn btn-outline">READ MORE</button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Featured;
+
 import SectionTitle from "../../../Shared/SectionTitle/SectionTitle";
-import featuredimg from "../../../../assets/home/featured.jpg"
+import featuredimg from "../../../../assets/home/featured.jpg";
+import img from "../../../../assets/home/featured.jpg"
 
 const Featured = () => {
+    const backgroundImageStyle = {
+        backgroundImage: `url(${img})`,
+        backgroundSize: "",
+        weight:"600px",
+        // opacity:".6",
+        // position:"relative",
+        backgroundPosition: "center",
+    };
+
     return (
-        <div>
+        <div className="" style={backgroundImageStyle}>
+            <div className="overlay"></div>
             <SectionTitle subHeading={"Check in Out"} heading={"Featured Items"}>
-                  
             </SectionTitle>
-            <div className=" flex">
-                <img src={featuredimg} className="w-[400px]" alt="" />
-                <div className="text-start">
+            <div className="flex justify-center items-center py-20 px-36">
+                <div className="image-container">
+                    <img src={featuredimg} className="relative" alt="" />
+                </div>
+                <div className="md:ml-10 text-start text-white">
                     <p className="">March 20, 2023</p>
                     <p className="">WHERE CAN I GET SOME?</p>
-                    <p className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, repellat. Est distinctio, magnam soluta dolore accusantium repudiandae ratione consequatur eius? Qui, expedita! At rerum consequuntur consectetur repellat harum unde temporibus!</p>
+                    <p className="">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Asperiores, repellat. Est distinctio, magnam soluta dolore accusantium
+                        repudiandae ratione consequatur eius? Qui, expedita! At rerum
+                        consequuntur consectetur repellat harum unde temporibus!
+                    </p>
                     <button className="btn btn-outline">READ MORE</button>
                 </div>
             </div>
